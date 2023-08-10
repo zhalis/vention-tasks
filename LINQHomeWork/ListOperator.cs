018ec6c168d126237e70b039175bb999d46a90b5
Vitali.Zhalis <zhalis19930109@gmail.com>
2023-08-10 21:15:19 +0300

    Added LINQ homework

---
+++ LINQHomeWork/ListOperator.cs
@@ -0,0 +1,64 @@
+namespace LINQHomeWork;
+
+public static class ListOperator
+{
+    public static List<int> GetListDividedByNumberWithoutRemainder(List<int> numbers, int divider)
+    {
+        return numbers
+            .Where(number => number % divider == 0)
+            .ToList();
+    }
+
+    /**
+     * Filters collection by range (rangeFrom - inclusively, rangeTo - exclusively)
+     */
+    public static List<int> FilterByRange(List<int> numbers, int rangeFrom, int rangeTo)
+    {
+        return numbers
+            .Where(number => number >= rangeFrom)
+            .Where(number => number < rangeTo)
+            .ToList();
+    }
+
+    public static Dictionary<int, int> SquareNumbers(List<int> numbers)
+    {
+        return numbers
+            .ToDictionary(num => num, num => num * num);
+    }
+
+    public static Dictionary<int, int> CalculateFrequencyOfNumbers(List<int> numbers)
+    {
+        Dictionary<int, int> frequency = new Dictionary<int, int>();
+        numbers
+            .ForEach(num => frequency.Add(num, frequency.Remove(num, out int value) ? ++value : 1));
+        return frequency;
+    }
+
+    public static List<string> FilterByStartAndEndCharacters(List<string> values, char start, char end)
+    {
+        return values
+            .Where(value => value.StartsWith(start) && value.EndsWith(end))
+            .ToList();
+    }
+
+    public static List<int> GetGreatestElements(List<int> values, int limit)
+    {
+        values.Sort((int firstNum, int secondNum) => secondNum.CompareTo(firstNum));
+        return values.GetRange(0, limit);
+    }
+
+    public static List<string> SortByLengthThenByAlphabet(List<string> values)
+    {
+        values.Sort((string1, string2) => string1.Length.CompareTo(string2.Length) == 0
+            ? string1.CompareTo(string2)
+            : string1.Length.CompareTo(string2.Length));
+        return values;
+    }
+
+    public static List<string> GetDistinctElementsInAscOrder(List<string> values)
+    {
+        values.Sort();
+        return values.Distinct()
+            .ToList();
+    }
+}
---
+++ LINQHomeWork/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace LINQHomeWork;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Func<string, int, string> concatIntToString = (acc, next) => acc + $" {next}";
+
+        Console.WriteLine("_________________________Task1_________________________________");
+        List<int> task1Numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int divider = 2;
+        Console.WriteLine(ListOperator.GetListDividedByNumberWithoutRemainder(task1Numbers, divider)
+            .Aggregate($"The numbers which produce the remainder 0 after divided by {divider} are:",
+                concatIntToString));
+
+        Console.WriteLine("_________________________Task2_________________________________");
+        List<int> task2Numbers = new List<int> { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+        int rangeFrom = 1;
+        int rangeTo = 11;
+        Console.WriteLine(string.Join(" ", ListOperator.FilterByRange(task2Numbers, rangeFrom, rangeTo)));
+
+
+        Console.WriteLine("_________________________Task3_________________________________");
+        List<int> task3Numbers = new List<int> { 9, 8, 6, 5 };
+        foreach (var keyValuePair in ListOperator.SquareNumbers(task3Numbers))
+        {
+            Console.WriteLine($"Number = {keyValuePair.Key}, SqrNo = {keyValuePair.Value}");
+        }
+
+
+        Console.WriteLine("_________________________Task4_________________________________");
+        List<int> task4Numbers = new List<int> { 1, 2, 3, 1, 2, 1, 3, 7, 3, 8 };
+        foreach (var keyValuePair in ListOperator.CalculateFrequencyOfNumbers(task4Numbers))
+        {
+            Console.WriteLine($"Number {keyValuePair.Key} appears {keyValuePair.Value} times");
+        }
+
+
+        Console.WriteLine("_________________________Task5_________________________________");
+        List<string> task5And7Cities = new List<string>
+        {
+            "ZURICH", "LONDON", "NAIROBI", "ROME", "CALIFORNIA", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS"
+        };
+        char start = 'A';
+        char end = 'M';
+        ListOperator.FilterByStartAndEndCharacters(task5And7Cities, start, end)
+            .ForEach(Console.WriteLine);
+
+
+        Console.WriteLine("_________________________Task6_________________________________");
+        List<int> task6Numbers = new List<int> { 5, 7, 13, 24, 6, 9, 8, 7 };
+        int limit = 3;
+        Console.WriteLine(ListOperator.GetGreatestElements(task6Numbers, limit)
+            .Aggregate($"The top {limit} records from the list are:", concatIntToString));
+
+
+        Console.WriteLine("_________________________Task7_________________________________");
+        ListOperator.SortByLengthThenByAlphabet(task5And7Cities)
+            .ForEach(Console.WriteLine);
+
+
+        Console.WriteLine("_________________________Task8_________________________________");
+        List<string> task8Words = new List<string>
+        {
+            "Butter", "Biscuit", "Brade", "Biscuit", "Honey", "Brade", "Biscuit", "Honey", "Butter"
+        };
+        ListOperator.GetDistinctElementsInAscOrder(task8Words)
+            .ForEach(Console.WriteLine);
+    }
+}
